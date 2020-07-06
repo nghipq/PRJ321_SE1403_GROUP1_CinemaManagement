3e5d9729a9f5d0f01dd741a6333689967bd7b993
nghipq <nghipqce140179@fpt.edu.vn>
2020-07-06 13:44:54 +0700

    add source 2

--- CinemaManagement/src/java/models/User.java
+++ CinemaManagement/src/java/models/User.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package controllers;
+package models;
 
 /**
  *
  * @author phamq
  */
-public class HomeController {
+public class User {
     
 }
