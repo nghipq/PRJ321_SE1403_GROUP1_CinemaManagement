b9db44bc9878b9230ea4ba317087138462ebf8f8
nghipq <nghipqce140179@fpt.edu.vn>
2020-07-08 16:07:03 +0700

    fix films

---
+++ template/public/OwlCarousel2-2.3.4/dist/owl.carouselOneSlide.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        dots: true,
+        nav:true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        responsive:{
+            0:{
+                items:1
+            },
+            420:{
+                items:2
+            },
+            600:{
+                item: 3    
+            },
+            1000:{
+                items:4
+            }
+        }
+    })   
+});
+
+window.onload = function() {
+    Particles.init({
+      selector: '.background',
+      color:'red',
+      speed: 1,
+      connectParticles: false
+    });
+  };  
